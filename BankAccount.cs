85476538098d03833500c6ccff0fd06ab49bc379
Kenneth Karani <Kenneth Karani@KARANI-KEN>
2023-07-03 12:58:08 +0300

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Banking_App
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Name { get; set; }
+        public decimal Balance { get; }
+
+        private static int accountNumberSeed = 1234567890;
+        public BankAccount( string name, decimal initialBalance)
+        {
+            this.Name = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            
+        }
+        public void makeDeposit()
+        {
+
+        }
+        public void makeWithdraw(string amount)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Banking_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Account = new BankAccount("Kenneth",1000);
+            Console.WriteLine($"An Account {Account.Number} has been created for " +
+                $"{Account.Name} with initial balance of {Account.Balance}");
+            Console.ReadLine();
+        }
+    }
+}
+                                                                        
