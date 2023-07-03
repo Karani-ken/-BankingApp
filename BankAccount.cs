fc1486bd45737470b4d52b042631313be5f0f216
Kenneth Karani <Kenneth Karani@KARANI-KEN>
2023-07-03 23:25:18 +0300

    created a transction class

--- BankAccount.cs
+++ BankAccount.cs
@@ -10,23 +10,52 @@ namespace Simple_Banking_App
     {
         public string Number { get; }
         public string Name { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance { 
+            
+            get 
+            {
+                decimal balance = 0;
+                    foreach (var item in Alltransactions) 
+                    {
+                             balance += item.Amount;
+                    }
+                    return balance;
+            }
+            
+        }
 
         private static int accountNumberSeed = 1234567890;
+        private List<Transactions> Alltransactions = new List<Transactions>();
         public BankAccount( string name, decimal initialBalance)
         {
             this.Name = name;
-            this.Balance = initialBalance;
+            makeDeposit(initialBalance, DateTime.Now, "initial balance");
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
             
         }
-        public void makeDeposit()
+        public void makeDeposit(decimal amount, DateTime date, string notes)
         {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount),"Amount of deposit must be positive");
+            }
+            var depost = new Transactions(amount, date, notes);
+            Alltransactions.Add(depost);
 
         }
-        public void makeWithdraw(string amount)
+        public void makeWithdraw(decimal amount, DateTime date, string notes)
         {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Withdrawal amount must bge positive");
+            }
+            if(Balance-amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this operation");
+            }
+            var withdrawa = new Transactions(amount, date, notes);
+            Alltransactions.Add(withdrawa);
 
         }
     }
--- Program.cs
+++ Program.cs
@@ -10,9 +10,13 @@ namespace Simple_Banking_App
     {
         static void Main(string[] args)
         {
-            var Account = new BankAccount("Kenneth",1000);
+            var Account = new BankAccount("Kenneth",10000);
             Console.WriteLine($"An Account {Account.Number} has been created for " +
                 $"{Account.Name} with initial balance of {Account.Balance}");
+            Account.makeDeposit(1500, DateTime.Now, "savings");
+            Console.WriteLine(Account.Balance);
+            Account.makeWithdraw(700, DateTime.Now, "Pizza");
+            Console.WriteLine(Account.Balance);
             Console.ReadLine();
         }
     }
